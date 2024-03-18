d6acf20bc9699f140fc6152982925dc2a95b65f9
TenP_256 <taiyo.yagyu.001@gmail.com>
2024-03-18 22:37:42 +0900

    3/18
    Homeは形にはなってきた。JS難しい。

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+//うまく動かないから未実装☆
+//js勉強しないと...
+//
+//点Pが動くやつのアニメーションが終わった後のタイムアウトの実装(をしたかった)
+//const TenPElement = document.querySelector(".TenP-animation");
+//let HanpukuCount = 0;
+//
+//animationendしたら、アニメーション一時停止
+//TenPElement.addEventListener("animationend", () => {
+//    HanpukuCount++;
+//   TenPElement.style.animationPlayState = "paused";
+//
+//2000ミリ秒=2sタイムアウトした後、アニメーション再生
+//     setTimeout(() => {
+//         TenPElement.style.animationPlayState = "running"
+//     }, 5000);
+// }
+// );
