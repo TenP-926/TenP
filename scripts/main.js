747e05dedb54c83bdca1781ce6f060c22049f71b
TenP_256 <taiyo.yagyu.001@gmail.com>
2024-04-04 21:02:18 +0900

    ダークモード対応
    ダークモードになるかはOSの設定に依存します。既知の問題: ホームアイコンが潰れる

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,5 @@
-//うまく動かないから未実装☆
-//js勉強しないと...
+//const btn = document.querySelector("#btn-mode-change");
 //
-//点Pが動くやつのアニメーションが終わった後のタイムアウトの実装(をしたかった)
-//const TenPElement = document.querySelector(".TenP-animation");
-//let HanpukuCount = 0;
+//const darkmodebyOS = window.matchMedia("(prefers-color-scheme: dark)");
+//const darkmode = darkmodebyOS.matches;
 //
-//animationendしたら、アニメーション一時停止
-//TenPElement.addEventListener("animationend", () => {
-//    HanpukuCount++;
-//   TenPElement.style.animationPlayState = "paused";
-//
-//2000ミリ秒=2sタイムアウトした後、アニメーション再生
-//     setTimeout(() => {
-//         TenPElement.style.animationPlayState = "running"
-//     }, 5000);
-// }
-// );
